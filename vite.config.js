cd6440d4846031af77c071ce74f05e97e6eeeb9f
Raphaël Nussbaumer <rafnuss@gmail.com>
2024-04-05 13:41:26 +0200

    update package and config to new website

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from "vite";
-//import vue from '@vitejs/plugin-vue'
-import { createVuePlugin as vue } from "vite-plugin-vue2";
+import vue from "@vitejs/plugin-vue2";
+//import { createVuePlugin as vue } from "vite-plugin-vue2";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue()],
-    base: "/KenyaBirdAtlasViz/",
+  plugins: [vue()],
+  base: "/",
 });
